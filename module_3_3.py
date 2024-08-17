c8672f14719a625504c2677cb368c43c6e8d1d40
RinazTurnin <52930266+RinazTurnin@users.noreply.github.com>
2024-08-17 08:20:40 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -1,6 +1,13 @@
 def print_params(a = 1, b = 'строка', c = True):
   print(a, b, c)
 
+print_params()
+print_params(2, 'строка', False)
+print_params('Rinaz', 'Done')
+print_params(50)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
 value_list = [5, False, 'привет']
 value_dict = {'a': 10, 'b': bool, 'c': 'привет'}
 print_params(*value_list)
